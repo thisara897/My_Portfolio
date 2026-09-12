5d206d7d865b5d6dcd89510827a8e11ea9c37016
thisara897 <warshangunasinghe345@gmail.com>
2026-09-12 23:00:07 +0530

    Add Three.js animation loop

--- app/components/Experience.jsx
+++ app/components/Experience.jsx
@@ -9,12 +9,46 @@ export default function Experience(){
     useEffect(()=>{
         const scene = new THREE.Scene();
 
-        const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 100);
-        scene.add(camera)
+                const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 100);
+                scene.add(camera)
+                camera.position.z = 3
 
-        const renderer = new THREE.WebGLRenderer({
-            canvas: canvasRef.current,
-    });
+                //Object
+                const geometry = new THREE.BoxGeometry(1, 1, 1);
+
+                const material = new THREE.MeshBasicMaterial({
+                    color: 0xff0000
+                })
+
+                const mesh = new THREE.Mesh(geometry, material);
+
+                scene.add(mesh);
+
+                const renderer = new THREE.WebGLRenderer({
+                    canvas: canvasRef.current,
+            });
+            renderer.setSize(window.innerWidth, window.innerHeight);
+            renderer.render(scene, camera)
+            //Clock
+
+            const clock = new THREE.Clock();
+
+            function tick(){
+                const deltaTime = clock.getDelta();
+                const elapsedTime = clock.getElapsedTime();
+
+                mesh.position.y = Math.sin(elapsedTime)
+
+                mesh.rotation.y += deltaTime * 0.5;
+
+                renderer.render(scene, camera);
+
+                window.requestAnimationFrame(tick)
+            }
+
+            tick();
+
+
         }, []);
 
 
